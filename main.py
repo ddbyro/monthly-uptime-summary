af44eb80fe548d43b9d76d655de5cbc610841725
ddbyro@gmail.com <ddbyro@gmail.com>
2023-08-11 23:01:30 -0600

    feat: added gunicorn for production wsgi, updated docs

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-from flask import Flask, render_template, abort, url_for, jsonify
+from flask import Flask, jsonify
 from prometheus_api_client import PrometheusConnect
-
+import yaml
 import datetime
 import json
 
+# a = gunicorn(__name__)
 app = Flask(__name__)
-prom = PrometheusConnect(url="http://localhost:9090", disable_ssl=True)
-
+config = yaml.full_load(open('./config/config.yaml'))
+prom = PrometheusConnect(url=f'{config["prod"]["hostname"]}:{config["prod"]["port"]}', disable_ssl=True)
 
 def get_uptime():
-    query = "sum(sum_over_time(probe_success{instance=~\".*\"}[30d])) / sum(count_over_time(probe_success{instance=~\".*\"}[30d])) * 100"
 
-    return prom.custom_query(query)
+    return prom.custom_query(config["prod"]["query"])
 
 
 def get_current_year():
@@ -24,34 +24,6 @@ def get_current_month():
     mydate = datetime.datetime.now()
 
     return mydate.strftime("%B")
-
-
-def create_json_package(year=None):
-    monthly_uptime = {'monthly-uptime':
-        [
-            {
-                'year': '',
-                'month': '',
-                'uptime': ''
-            }
-        ]
-    }
-
-    monthly_uptime.update(
-        {'monthly-uptime':
-            [
-                {
-                    'year': f'{year}',
-                    'month': f'{get_current_month()}',
-                    'uptime': f'{get_uptime()[0]["value"][1]}'
-                }
-            ]
-        }
-    )
-
-    json_pkg = json.dumps(monthly_uptime, indent=4)
-
-    return json_pkg
 
 
 @app.route("/json/")
